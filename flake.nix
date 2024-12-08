b32a997373592775cfeb06487d4c1dedd91bca3a
Tristan Mc Leay <tristan@cassowary.org>
2024-12-09 10:29:05 +1100

    flake for easier running

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let read-src-asset = legacyPax: legacyPax.haskellPackages.callCabal2nix "read-src-asset" ./read-src-asset {};
+  in {
+
+    packages.x86_64-linux.default = read-src-asset nixpkgs.legacyPackages.x86_64-linux;
+    packages.aarch64-darwin.default = read-src-asset nixpkgs.legacyPackages.aarch64-darwin;
+
+
+  };
+}
