04891710cc9dce7a66bd5653a91c050f6517830e
Tristan Mc Leay <tristan@cassowary.org>
2025-02-03 14:00:05 +1100

    add an export

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
   let read-src-asset = legacyPax: legacyPax.haskellPackages.callCabal2nix "read-src-asset" ./read-src-asset {};
   in {
 
+    export = read-src-asset;
+
     packages.x86_64-linux.default = read-src-asset nixpkgs.legacyPackages.x86_64-linux;
     packages.aarch64-darwin.default = read-src-asset nixpkgs.legacyPackages.aarch64-darwin;
 
